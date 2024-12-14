4a61f671d960416f7676452c38ad39f7417e8ee9
zkhumalo <156356610+zkhumalo@users.noreply.github.com>
2024-12-13 21:45:23 -0500

    Update mtp2.py

---
+++ mtp2.py
@@ -0,0 +1,6 @@
+# Import needed libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the NYC-pop1.csv file and plot the data of Queens
+nyc_pop1 = pd.read_csv('NYC-pop1.csv', header=5)
